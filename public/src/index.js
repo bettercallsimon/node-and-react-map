e5ee7ac23ad3ecd45e30a638b184b03859d39286
Simon Charette <bettercallsimon@BetterCallSimon.local>
2018-01-08 06:46:42 -0500

    service worker disabled, basic hello world

--- public/src/index.js
+++ public/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker'; => god damn service worker during production...
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+//  service workers, disabled for the moment + making sure I unregister them all for this app at the moment
+//registerServiceWorker();
+unregister();
