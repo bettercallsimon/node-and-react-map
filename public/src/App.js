b7f8a686501d3c68ef9646105300146fc8dbaa96
Simon Charette <bettercallsimon@BetterCallSimon.local>
2018-01-10 08:15:37 -0500

    remade the client component structure

--- public/src/App.js
+++ public/src/App.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import MenuBarTitle from './components/menu_bar_title';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {status : 'idle', appTitle: 'none'};
+
+
+
+	}
+	
 	render() {
 		return (
 			<div className="App">
 			{/* * THE ACTUAL CLIENT CODE */}
 
 				<div class="appWrapper">
-					<div class="interfaceWrapper">
-						
-							<p>hello world</p>
-							<div class="flex-item">1</div>
-							<div class="flex-item">2</div>
-							<div class="flex-item">3</div>
-							<div class="flex-item">4</div>
 
-					</div>
+					{/* === APP STRUCTURE === */}
+					
+					{/* menu_bar_title */}
+						{/* pop_notification */}
+						{/* pop_account */}
+						{/* pop_logout */}
+
+					{/* IF (LOGGED) map ELSE login */}
+						{/* pop_filter */}
+							{/* child of map: pop_building */}
+							{/* child of map: pop_employee */}
+
+					{/* menu_bar_footer */}
+						{/* menu_newsfeed */}
+						{/* pop_contactbook */}
+						{/* pop_addressbook */}
+						{/* pop_intelbook */}
+			
+					{/* temporary code just to see if it works */}
+					<MenuBarTitle appTitle={this.state.appTitle} />
+
 				</div>
 			
 			{/* end of App's main div */}
---
+++ public/src/components/menu_bar_title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MenuBarTitle = (props) => {
+    return (
+        <div className="">
+            <h1>{props.appTitle}</h1>
+        </div>
+    )
+};
+
+export default MenuBarTitle;
