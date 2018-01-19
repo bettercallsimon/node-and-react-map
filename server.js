abc15d239cfd915fe414ddbc25f6cc5c20343d6c
Simon Charette <hoaxchannel@gmail.com>
2018-01-18 20:05:15 -0500

    rested back to normal

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //  send traffic to the route/index.js file where express.Router() handle every requests
-// yeah!
 app.use('/', routes);
 
 //  a very basic way to start to listen, could be upgraded
