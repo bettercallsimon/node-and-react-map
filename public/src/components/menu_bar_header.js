b273e58fe61a2ac62bb52b582d154f67d3566e7b
bettercallsimon <hoaxchannel@gmail.com>
2018-01-26 20:15:52 -0500

    added nav element to menu and css refact

--- public/src/components/menu_bar_header.js
+++ public/src/components/menu_bar_header.js
@@ -3,7 +3,23 @@ import React from 'react';
 const MenuBarHeader = (props) => {
     return (
         <div className="menu_bar_header">
-            <h1>{props.appTitle}</h1>
+           <div className="menu_bar_header_layout">
+                <div className="menu_bar_header_title">
+                        <h1>{props.appTitle}</h1>
+                </div>
+                <div className="menu_bar_header_nav">
+                    <div className="menu_bar_header_icon">
+
+                    </div>
+                    <div className="menu_bar_header_icon">
+
+                    </div>
+                    <div className="menu_bar_header_icon">
+
+                    </div>
+
+                </div>
+           </div>
         </div>
     )
 };
