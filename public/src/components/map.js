1f66c5305e90bc384e543bfb5818961fbef9c6fb
bettercallsimon <hoaxchannel@gmail.com>
2018-01-29 06:53:41 -0500

    added basic map style support, need refactor the map theme in a separate file

--- public/src/components/map.js
+++ public/src/components/map.js
@@ -7,6 +7,8 @@ import axios from 'axios';
 
 import Building from './pop_building';
 
+// const mapTheme = require("./helpers/mapTheme.json");
+
 
 
 class Map extends Component {
@@ -17,7 +19,8 @@ class Map extends Component {
         this.state = {
             mapData : [], 
             centerPosition : {lat: 30.95, lng: 5.33},
-            zoom : 2 
+            zoom : 2, 
+            mapTheme: {styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]}
         };
 
     }
@@ -62,9 +65,9 @@ class Map extends Component {
                 //  MAP INIT.
                 center={this.state.centerPosition}
                 zoom={this.state.zoom}
+                // defaultOptions={{ styles: mapTheme }}
+                options={this.state.mapTheme}
                 className="map"
-                onChildClick={console.log(this)}
-                
             >
 
             {/* ====== components on the map at long-lat  ====== */}
@@ -87,8 +90,12 @@ class Map extends Component {
 		})
 		.catch(function (error) {
 		    console.log(error);
-		});
+        });
+
+        console.log("mapStyle", this.state.mapStyle);
 	}
 }
+
+
 
 export default Map;
